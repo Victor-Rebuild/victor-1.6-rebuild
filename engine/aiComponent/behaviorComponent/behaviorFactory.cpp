8a0ed6227cad30c1475143caf022555946a12f01
Ron Barry <ronald.barry@anki.com>
2019-01-08 18:12:50 -0800

    Add the ability to drive Vector using the Cube.

--- engine/aiComponent/behaviorComponent/behaviorFactory.cpp
+++ engine/aiComponent/behaviorComponent/behaviorFactory.cpp
@@ -57,6 +57,7 @@
 #include "engine/aiComponent/behaviorComponent/behaviors/danceToTheBeat/behaviorDanceToTheBeat.h"
 #include "engine/aiComponent/behaviorComponent/behaviors/danceToTheBeat/behaviorDanceToTheBeatCoordinator.h"
 #include "engine/aiComponent/behaviorComponent/behaviors/danceToTheBeat/behaviorListenForBeats.h"
+#include "engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorCubeDrive.h"
 #include "engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorDevBatteryLogging.h"
 #include "engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorDevCubeSpinnerConsole.h"
 #include "engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorDevDesignCubeLights.h"
@@ -495,6 +496,12 @@ ICozmoBehaviorPtr BehaviorFactory::CreateBehavior(const Json::Value& config)
       break;
     }
     
+    case BehaviorClass::CubeDrive:
+    {
+      newBehavior = ICozmoBehaviorPtr(new BehaviorCubeDrive(config));
+      break;
+    }
+
     case BehaviorClass::DevBatteryLogging:
     {
       newBehavior = ICozmoBehaviorPtr(new BehaviorDevBatteryLogging(config));
