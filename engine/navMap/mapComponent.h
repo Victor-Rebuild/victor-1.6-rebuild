e795001146251558d1b0792a67104bd5c12ba653
Matt Michini <mmichini@anki.com>
2019-01-16 15:00:34 -0800

    Add new ReactToBoundary to react to 'invisible' existing boundaries in the nav map

--- engine/navMap/mapComponent.h
+++ engine/navMap/mapComponent.h
@@ -146,6 +146,9 @@ public:
   
   void SendDASInfoAboutCurrentMap() const { SendDASInfoAboutMap(_currentMapOriginID); }
   
+  // Helper method to retrieve all boundaries in the map
+  Result FindBoundaries(std::vector<MemoryMapTypes::MemoryMapDataConstPtr>& boundaries) const;
+
   ////////////////////////////////////////////////////////////////////////////////
   // Accessors
   ////////////////////////////////////////////////////////////////////////////////
