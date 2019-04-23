071f0a770ba1b9ffb841a3f81f41928e22df0cb5
Patrick Doran <patrick.doran@anki.com>
2019-04-23 10:43:24 -0700

    Addressing code review (1)

--- coretech/vision/engine/imageBuffer/imageBuffer.cpp
+++ coretech/vision/engine/imageBuffer/imageBuffer.cpp
@@ -229,7 +229,8 @@ bool ImageBuffer::GetRGBFromBAYER(ImageRGB& rgb, ImageCacheSize size) const
   rgb.Allocate(outHeight, outWidth);
   Debayer::OutArgs outArgs(rgb.get_CvMat_().data, outHeight, outWidth, outScale, outFormat);
 
-  return Debayer::Instance().Invoke(method, inArgs, outArgs) == RESULT_OK;
+  Result res = Debayer::Instance().Invoke(method, inArgs, outArgs);
+  return res == RESULT_OK;
 }
 
 bool ImageBuffer::GetRGBFromRawRGB(ImageRGB& rgb, ImageCacheSize size) const
@@ -331,7 +332,8 @@ bool ImageBuffer::GetGrayFromBAYER(Image& gray, ImageCacheSize size) const
   gray.Allocate(outHeight, outWidth);
   Debayer::OutArgs outArgs(gray.get_CvMat_().data, outHeight, outWidth, outScale, outFormat);
 
-  return Debayer::Instance().Invoke(method, inArgs, outArgs) == RESULT_OK;
+  Result res = Debayer::Instance().Invoke(method, inArgs, outArgs);
+  return res == RESULT_OK;
 }
 
 bool ImageBuffer::GetGrayFromRawRGB(Image& gray, ImageCacheSize size) const
