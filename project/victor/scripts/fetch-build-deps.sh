5a13b5246ed82aa577a04bd631dfd87f31b6c3cf
Switch-modder <deolrajjyot@gmail.com>
2025-08-26 21:59:43 -0400

    Make EXTERNALS into a submodule (Part 3)

--- project/victor/scripts/fetch-build-deps.sh
+++ project/victor/scripts/fetch-build-deps.sh
@@ -7,7 +7,7 @@ SCRIPT_PATH=$(dirname $([ -L $0 ] && echo "$(dirname $0)/$(readlink -n $0)" || e
 set +e
 CURRENT_EXTERNALS_VERSION=$(cat "EXTERNALS/VERSION") >> /dev/null
 set -e
-EXTERNALS_VERSION_LATEST=$(curl https://raw.githubusercontent.com/Switch-modder/victor-1.6-rebuild-externals/refs/heads/main/VERSION) >> /dev/null
+EXTERNALS_VERSION_LATEST=7
 
 GIT=`which git`
 if [ -z $GIT ]; then
@@ -35,19 +35,28 @@ esac
 
 if [[ -d EXTERNALS/ ]]; then
     if [ $EXTERNALS_UPDATE_SKIP != 1 ]; then
-        if [ "$CURRENT_EXTERNALS_VERSION" != "$EXTERNALS_VERSION_LATEST" ]; then
-            echo "Old EXTERNALS version found"
-            echo "Updating EXTERNALS"
-            cd EXTERNALS/
-            git pull
+        if [ "$CURRENT_EXTERNALS_VERSION" -le "6" ]; then
+            echo "This is a old version of EXTERNALS from before it was a submodule"
+            echo "Please reclone the repo in order to get the submodule"
+            echo "Current version = $CURRENT_EXTERNALS_VERSION"
+            echo "Latest version = $EXTERNALS_VERSION_LATEST "
+            exit 1
         else
-            echo "EXTERNALS up to date (Latest version = $EXTERNALS_VERSION_LATEST)"
+            if [ "$CURRENT_EXTERNALS_VERSION" != "$EXTERNALS_VERSION_LATEST" ]; then
+                echo "Old EXTERNALS version found"
+                echo "Updating EXTERNALS"
+                cd EXTERNALS/
+                git pull
+            else
+                echo "EXTERNALS up to date (EXTERNALS version = $CURRENT_EXTERNALS_VERSION, Latest version = $EXTERNALS_VERSION_LATEST)"
+            fi
         fi
     else
         echo "EXTERNALS update skip detected, not updating EXTERNALS"
     fi
 else
     echo "This repo was cloned incorrectly, please reclone with the "--recurse-submodules" flag"
+    exit 1
 fi
 
 HOST_FETCH_DEPS=${SCRIPT_PATH}/"fetch-build-deps.${HOST}.sh"
