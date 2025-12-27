29ac6ecb29a08e3954f52f229e3e88b6ee3becba
Switch-modder <deolrajjyot@gmail.com>
2025-12-27 12:53:57 -0500

    Major knowledge/intent graph speedup

--- project/victor/build-victor.sh
+++ project/victor/build-victor.sh
@@ -525,7 +525,7 @@ fi
 
 # copied from build-v.sh
 cd $TOPLEVEL
-echo "Copying vic-cloud and vic-gateway..."
+echo "Copying vic-cloud..."
 cp -a bin/* _build/vicos/Release/bin/
 echo "Copying libopus..."
 cp -a EXTERNALS/deps/opus/libopus.so.0.7.0 _build/vicos/Release/lib/libopus.so.0
