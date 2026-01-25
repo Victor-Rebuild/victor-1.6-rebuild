85eb1bedf0b86390b3a254c892ba45394ed94afd
Switch-modder <deolrajjyot@gmail.com>
2026-01-25 12:01:57 -0500

    Complete VIC-13319

--- animProcess/src/cozmoAnim/alexa/alexa.cpp
+++ animProcess/src/cozmoAnim/alexa/alexa.cpp
@@ -44,8 +44,8 @@ namespace Anki {
 namespace Vector {
   
 // VIC-13319 remove
-CONSOLE_VAR(bool, kAlexaEnabledInUK, "Alexa", true);
-CONSOLE_VAR(bool, kAlexaEnabledInAU, "Alexa", true);
+// CONSOLE_VAR(bool, kAlexaEnabledInUK, "Alexa", true);
+// CONSOLE_VAR(bool, kAlexaEnabledInAU, "Alexa", true);
   
 namespace {
   const std::string kAlexaPath = "alexa";
@@ -67,9 +67,9 @@ namespace {
     if( locale.GetCountry() == Util::Locale::CountryISO2::US ) {
       return true;
     } else if( locale.GetCountry() == Util::Locale::CountryISO2::GB ) {
-      return kAlexaEnabledInUK;
+      return true;
     } else if( locale.GetCountry() == Util::Locale::CountryISO2::AU ) {
-      return kAlexaEnabledInAU;
+      return true;
     } else {
       return false;
     }
