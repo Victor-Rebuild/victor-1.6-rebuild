ffd1be73be2fc669c07129fa23d8f7abd09c33e4
Switch-modder <deolrajjyot@gmail.com>
2026-01-25 15:01:17 -0500

    Add debug stuff for build commands

--- setenv.sh
+++ setenv.sh
@@ -9,10 +9,14 @@ alias victor_restart="cd ${GIT_PROJ_ROOT} && ${GIT_PROJ_ROOT}/project/victor/scr
 alias victor_start="cd ${GIT_PROJ_ROOT} && ${GIT_PROJ_ROOT}/project/victor/scripts/victor_start.sh"
 alias victor_stop="cd ${GIT_PROJ_ROOT} && ${GIT_PROJ_ROOT}/project/victor/scripts/victor_stop.sh"
 alias victor_stage="cd ${GIT_PROJ_ROOT} && ${GIT_PROJ_ROOT}/project/victor/scripts/stage.sh -c Release -b"
+alias victor_stage_debug="cd ${GIT_PROJ_ROOT} && ${GIT_PROJ_ROOT}/project/victor/scripts/stage.sh -c Debug -b"
 alias watch_backtraces="cd ${GIT_PROJ_ROOT} && echo Waiting for backtraces... && python3 project/victor/scripts/backtracewatcher.py"
+alias watch_backtraces_debug="cd ${GIT_PROJ_ROOT} && echo Waiting for backtraces... && python3 project/victor/scripts/backtracewatcherdebug.py"
 
 alias victor_build_release="cd ${GIT_PROJ_ROOT} && ${GIT_PROJ_ROOT}/project/victor/scripts/victor_build_release.sh"
+alias victor_build_debug="cd ${GIT_PROJ_ROOT} && ${GIT_PROJ_ROOT}/project/victor/scripts/victor_build_debugo2.sh"
 alias victor_deploy_run="cd ${GIT_PROJ_ROOT} && ${GIT_PROJ_ROOT}/project/victor/scripts/victor_deploy.sh -c Release -b && ${GIT_PROJ_ROOT}/project/victor/scripts/victor_start.sh"
+alias victor_deploy_run_debug="cd ${GIT_PROJ_ROOT} && ${GIT_PROJ_ROOT}/project/victor/scripts/victor_deploy.sh -c Debug -b && ${GIT_PROJ_ROOT}/project/victor/scripts/victor_start.sh"
 
 function IS_ROBOT_IP_THERE() {
     if [[ ! -f "$GIT_PROJ_ROOT/robot_ip.txt" ]]; then
@@ -25,8 +29,11 @@ function IS_ROBOT_IP_THERE() {
 
 alias vclean='rm -rf ${GIT_PROJ_ROOT}/_build ${GIT_PROJ_ROOT}/generated'
 alias vbuild=victor_build_release
+alias vbuilddebug=victor_build_debug
 alias vdeploy='IS_ROBOT_IP_THERE; victor_stage && victor_deploy_run'
+alias vdeploydebug='IS_ROBOT_IP_THERE; victor_stage_debug && victor_deploy_run_debug'
 alias vbd='victor_build_release && victor_stage && victor_deploy_run'
+alias vbdd='victor_build_debug && victor_stage_debug && victor_deploy_run_debug'
 alias stage='victor_stage'
 alias see_size_of_silly_victor_build='victor_stage && du -sh _build/staging'
 alias wbtrace='watch_backtraces'
